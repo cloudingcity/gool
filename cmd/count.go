02b1585add933af8b8664ce6b6bcf77e68c851a8
clouding <ghost0436@gmail.com>
2020-09-04 01:16:50 +0800

    feat: add count cmd

---
+++ cmd/count.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var countCmd = &cobra.Command{
+	Use:                   "count [text]",
+	Short:                 "Count",
+	Args:                  cobra.ExactArgs(1),
+	DisableFlagsInUseLine: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(len(strings.TrimSpace(args[0])))
+	},
+}

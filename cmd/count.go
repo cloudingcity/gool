96ce5b9af9f05f458b48edf7a2eb0c071551da79
clouding <ghost0436@gmail.com>
2020-09-04 22:02:43 +0800

    refactor: modify count cmd descriptino

--- cmd/count.go
+++ cmd/count.go
@@ -9,7 +9,7 @@ import (
 
 var countCmd = &cobra.Command{
 	Use:                   "count [text]",
-	Short:                 "Count",
+	Short:                 "Get the characters length",
 	Args:                  cobra.ExactArgs(1),
 	DisableFlagsInUseLine: true,
 	Run: func(cmd *cobra.Command, args []string) {
